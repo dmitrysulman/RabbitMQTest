c0c7fe969803ad90a7eb5dc86da1c8c9fbd53ccf
Dmitry Sulman <dmitry.sulman@gmail.com>
2022-10-20 20:40:26 +0300

    commit 2

--- src/main/java/org/dmitrysulman/rabbitmq/Recv.java
+++ src/main/java/org/dmitrysulman/rabbitmq/Recv.java
@@ -8,21 +8,45 @@ import com.rabbitmq.client.DeliverCallback;
 import java.nio.charset.StandardCharsets;
 
 public class Recv {
-    private final static String QUEUE_NAME = "hello";
+    private final static String QUEUE_NAME = "hello3";
 
     public static void main(String[] args) throws Exception {
         ConnectionFactory factory = new ConnectionFactory();
         factory.setHost("localhost");
         Connection connection = factory.newConnection();
         Channel channel = connection.createChannel();
-        channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+        boolean durable = true;
+        channel.queueDeclare(QUEUE_NAME, durable, false, false, null);
 
         System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
 
         DeliverCallback deliverCallback = (consumerTag, delivery) -> {
             String message = new String(delivery.getBody(), StandardCharsets.UTF_8);
             System.out.println(" [x] Received '" + message + "'");
+
+            try {
+                doWork(message);
+            } finally {
+                System.out.println(" [x] done");
+                channel.basicAck(delivery.getEnvelope().getDeliveryTag(), false);
+            }
         };
-        channel.basicConsume(QUEUE_NAME, true, deliverCallback, consumerTag -> {});
+
+        int prefetchCount = 1;
+        channel.basicQos(prefetchCount);
+        boolean autoAck = false;
+        channel.basicConsume(QUEUE_NAME, autoAck, deliverCallback, consumerTag -> {});
+    }
+
+    private static void doWork(String task){
+        for (char ch: task.toCharArray()) {
+            if (ch == '.') {
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                }
+            }
+        }
     }
 }
--- src/main/java/org/dmitrysulman/rabbitmq/Send.java
+++ src/main/java/org/dmitrysulman/rabbitmq/Send.java
@@ -3,18 +3,24 @@ package org.dmitrysulman.rabbitmq;
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.MessageProperties;
+
+import java.nio.charset.StandardCharsets;
 
 public class Send {
-    private final static String QUEUE_NAME = "hello";
+    private final static String QUEUE_NAME = "hello3";
 
     public static void main(String[] args) throws Exception {
         ConnectionFactory factory = new ConnectionFactory();
         factory.setHost("localhost");
         try (Connection connection = factory.newConnection();
              Channel channel = connection.createChannel()) {
-            channel.queueDeclare(QUEUE_NAME, false, false, false, null);
-            String message = "Hello world!";
-            channel.basicPublish("", QUEUE_NAME, null, message.getBytes());
+            boolean durable = true;
+            channel.queueDeclare(QUEUE_NAME, durable, false, false, null);
+            String message = String.join(" ", args);
+            channel.basicPublish("", QUEUE_NAME,
+                    MessageProperties.PERSISTENT_TEXT_PLAIN,
+                    message.getBytes(StandardCharsets.UTF_8));
             System.out.println(" [x] Sent '" + message + "'");
         }
     }
