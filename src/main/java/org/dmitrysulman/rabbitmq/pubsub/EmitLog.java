12b9b12fc05f32bd8cee06671621f643cad20945
Dmitry Sulman <dmitry.sulman@gmail.com>
2022-10-25 14:53:03 +0300

    pubsub

---
+++ src/main/java/org/dmitrysulman/rabbitmq/pubsub/EmitLog.java
@@ -0,0 +1,25 @@
+package org.dmitrysulman.rabbitmq.pubsub;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+import java.nio.charset.StandardCharsets;
+
+public class EmitLog {
+    private static final String EXCHANGE_NAME = "logs";
+
+    public static void main(String[] args) throws Exception {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        try (Connection connection = factory.newConnection();
+             Channel channel = connection.createChannel()) {
+            channel.exchangeDeclare(EXCHANGE_NAME , "fanout");
+
+            String message = args.length < 1 ? "Hello world!" : String.join(" ", args);
+
+            channel.basicPublish(EXCHANGE_NAME , "", null, message.getBytes(StandardCharsets.UTF_8));
+            System.out.println(" [x] Sent '" + message + "'");
+        }
+    }
+}
